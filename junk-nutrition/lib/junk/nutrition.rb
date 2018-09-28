7c2d002a07d89116c7d51d12f9f58e79429fec04
Rima Jean <rima.jean@gmail.com>
2018-09-28 16:20:56 +0000

    automatically backed up by learn

---
+++ junk-nutrition/lib/junk/nutrition.rb
@@ -0,0 +1,7 @@
+require "junk/nutrition/version"
+
+module Junk
+  module Nutrition
+    # Your code goes here...
+  end
+end
---
+++ junk-nutrition/lib/junk/nutrition/version.rb
@@ -0,0 +1,5 @@
+module Junk
+  module Nutrition
+    VERSION = "0.1.0"
+  end
+end
